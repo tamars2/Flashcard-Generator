91b42a6c5b5d5b83b0bd33b3f57cdfc4c41e4cc3
Trent Marshall <Trent@Sarahs-MacBook-Air-2.local>
2017-04-14 23:39:41 -0400

    v1.2

--- flashcards.js
+++ flashcards.js
@@ -1,92 +1,110 @@
+//required node packages
 var inquirer = require("inquirer");
 var fs = require("fs");
 
-
-//create basic card
-function BasicCard(front, back){
+//object constructor for basic card
+function BasicCard(front, back) {
     this.front = front;
     this.back = back;
+    //create method makes the basic card object
+    this.create = function() {
+		var cardData = {
+			type: "basic",
+			front: this.front,
+			back: this.back
+    	};
+    	//write the object to .txt file
+    	writeFile(cardData);
+    }
 }
-//create Cloze card
+//object constructor for Cloze card
 function ClozeCard(text, cloze){
     this.text = text;
     this.cloze = cloze;
+    //replace cloze word with ellipsis
+    this.clozeDeleted = this.text.replace(this.cloze, ' ... ');
+    //create method makes the cloze card object
+    this.create = function() {
+    	var cardData = {
+    		type: "cloze",
+    		text: this.text,
+    		cloze: this.cloze,
+    		clozeDeleted: this.clozeDeleted
+    		
+    	};
+    	//write the object to .txt file
+    	writeFile(cardData); 
+    }
 }
-
-//make a partial prototype for cloze type cards
-ClozeCard.prototype.partial = function () {
-	//if the card has prototype of partial, replace cloze
-	//string with ellipsis
-    if(this.text.includes(this.cloze)){
-        return this.text.replace(this.cloze, ' ... ');
-    }
-    else{
-        return "ERROR";
-    }
-};
-
-
-//prompt the user for card type
-inquirer.prompt({
-    type: 'list',
-    name: 'cards',
-    message: 'Choose your card?',
-    choices: ['BasicCard', 'ClozureCard']
-}).then(function (data) {
-	//if user choses basic, ask for font and back info
-    if(data.cards === 'BasicCard'){
-        return inquirer.prompt([
-            {
+//this is the API that prompts user for card info, creates cards and logs them
+function addCards() {
+	inquirer.prompt({
+	    type: 'list',
+	    name: 'cards',
+	    message: 'Which type of card would you like to create? (press control^c to exit)',
+	    choices: ['BasicCard', 'ClozeCard']
+	}).then(function (data) {
+		//if user choses basic, ask for font and back info
+		if(data.cards === 'BasicCard'){
+        	return inquirer.prompt([
+       	    {
                 type: 'input',
                 name: 'front',
                 message: 'What do you want on the front of the card?'
-            }, {
+            }, 
+            {
                 type: 'input',
                 name: 'back',
                 message: 'What do you want on the back of the card?'
             }
-        ]);
-    }
-
-    else{
-    	//else ask for cloze card info
-        return inquirer.prompt([
-            {
-                type: 'input',
-                name: 'text',
-                message: 'What do you want on your card?'
-            }, {
-                type: 'input',
-                name: 'cloze',
-                message: 'What would you like deleted for your cloze card?'
-            }
-        ]);
-    }
+        	]);
+    	}
+    	else{
+    		//else ask for cloze card info
+    	    return inquirer.prompt([
+    	        {
+    	            type: 'input',
+    	            name: 'text',
+    	            message: 'What do you want on your card?'
+    	        }, 
+    	        {
+        	        type: 'input',
+            	    name: 'cloze',
+					message: 'What would you like deleted from your cloze card?'
+            	}
+        	]);
+    	}
     //then make the cards using object Constructors
 }).then(function(data){
-		//construct basic card
-        if(data.front){
+		//construct basic card and begin input validation
+        if (data.front && data.back){
             var basic = new BasicCard(data.front, data.back);
-            addCards({data: basic});
+            basic.create();
+            console.log("Your Basic card has been added to the deck!");
+            addCards();
 
         }
-        //construct cloze card
-        else{
+        //construct cloze card and continue input validation
+        else if (data.text && data.cloze) {
             var cloze = new ClozeCard(data.text, data.cloze);
-            addCards({data: cloze, partial: cloze.partial()});
+            cloze.create();
+            console.log("Your Cloze card has been added to the deck!");
+            addCards();
         }
-
-
+        else {
+        	console.log('You must fill out each side of the card!  Please try again.');
+		 	addCards();
+        }
     })
 	//catch and log errors should they happen
     .catch(function (err) {
      console.log(err);
 });
-//add cards to the cards json file for use later
-var addCards = function (add) {
-	console.log(add);
-
-    fs.appendFile('./basic.json', "\r\n" + JSON.stringify(add), 'utf8', function (error, data) {
-    	console.log("Your card has been added to the deck!");
-   }); 
-};
+}
+//function for writing data to the .txt file
+function writeFile(cardData) {
+	fs.appendFile('./log.txt', "\r\n" + JSON.stringify(cardData), 'utf8', function (error, data) {
+   		});
+}
+//run the API at least one time
+addCards();
